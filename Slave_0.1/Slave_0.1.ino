65aace08cb4e8b23c7003e5ea93e7da7232cfc58
EA890 <ea890@live.mdx.ac.uk>
2017-12-14 20:20:13 +0000

    Added sensor input constants

--- Slave_0.1/Slave_0.1.ino
+++ Slave_0.1/Slave_0.1.ino
@@ -1,9 +1,25 @@
 //Sprinkler Outputs:
-const int sprinkler_One = 15;
-const int sprinkler_Two = 27;
-const int sprinkler_Three = 10;
+const int sprinkler_One = 18;
+const int sprinkler_Two = 24;
+const int sprinkler_Three = 15;
 
-//Moisture_one = 
+//Moisture Probes
+const int moisture_One = A1;
+const int moisture_Two = A2;
+const int moisture_Three = A3;
+
+//Temperature Sensor
+const int temp_One = A4;
+const int temp_Two = A5;
+const int temp_Three = A6;
+
+//Heater
+const int heater = 11;
+
+//Emergancy Stop
+const int E_Stop = 10;
+volatile bit emergancy = LOW;
+
 
 void setup() {
   // put your setup code here, to run once:
