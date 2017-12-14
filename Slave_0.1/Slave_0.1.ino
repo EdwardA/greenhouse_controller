2520fef67b6017eba05c20ae37daa2b86e45205e
EA890 <ea890@live.mdx.ac.uk>
2017-12-14 17:52:20 +0000

    Added sprinkler pin constants to ardunio sketch

---
+++ Slave_0.1/Slave_0.1.ino
@@ -0,0 +1,16 @@
+//Sprinkler Outputs:
+sprinkler_One = 15;
+sprinkler_Two = 27;
+sprinkler_Three = 10;
+
+//Moisture_one = 
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
